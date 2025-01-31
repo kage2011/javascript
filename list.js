fa9c3b29509ab1dc56c30bf846af5b474c61569d
kage2011 <82596704+kage2011@users.noreply.github.com>
2025-01-31 16:18:06 +0900

    Update list.js

--- list.js
+++ list.js
@@ -23,6 +23,7 @@ window.addEventListener('load', function() {
         saveButton.textContent = '保存';
         saveButton.style.backgroundColor = 'lime'; // ボタンの色を緑に設定
         saveButton.style.marginLeft = '10px'; // 左側にスペースを追加
+        saveButton.style.margintop = '10px'; // スペースを追加
         var title = document.querySelectorAll('.kb-injector-header-title');
         title[0].appendChild(saveButton);
     
