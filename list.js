e997c26b17afb6f2a0256bfd0bb3ee30e04336b2
kage2011 <82596704+kage2011@users.noreply.github.com>
2025-01-31 15:52:46 +0900

    Update list.js

--- list.js
+++ list.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', function() {
+    // デバイス特有の情報を取得（ここではユーザーエージェントを使用）
+    var deviceInfo = navigator.userAgent;
     setTimeout(function() {
         // 前回保存されたデータをlocalStorageから読み込む
-        var savedData = localStorage.getItem('inputData');
+        var savedData = localStorage.getItem('inputData${deviceInfo}');
         if (savedData) {
             var data = JSON.parse(savedData);
-            var inputs = document.querySelectorAll('input');
+            var inputs = document.querySelectorAll('input, select');
             var i = 0;
             data.forEach(function(item) {
                 var inputField = inputs[i];
@@ -24,22 +26,24 @@ window.addEventListener('load', function() {
     
         // ボタンがクリックされたときの処理を追加
         saveButton.addEventListener('click', function() {
-            // IDに'input'を含むすべてのinputタグを取得
-            var inputFields = document.querySelectorAll('input');
-            var data = [];
-    
-            // 各inputタグのclassと値をセットにしたオブジェクトを作成
-            inputFields.forEach(function(inputField) {
-                data.push({
-                    id: inputField.id,
-                    class: inputField.className,
-                    value: inputField.value
+            // 警告を表示してユーザーに確認
+            var confirmSave = confirm('共有のデバイス（職場のパソコンなど）では保存したデータが第三者に見られる危険があります。それでも保存しますか？');
+            if (confirmSave) {
+                // IDに'input'を含むすべてのinputタグを取得
+                var inputFields = document.querySelectorAll('input, select');
+                var data = [];
+        
+                // 各inputタグのclassと値をセットにしたオブジェクトを作成
+                inputFields.forEach(function(inputField) {
+                    data.push({
+                        value: inputField.value
+                    });
                 });
-            });
     
-            // データをlocalStorageに保存
-            localStorage.setItem('inputData', JSON.stringify(data));
-            alert('データが保存されました');
+                // データをlocalStorageに保存
+                localStorage.setItem('inputData', JSON.stringify(data));
+                alert('データが保存されました');
+            }
         });
     }, 2000);
 });
