c4747298e7a3df5e98cd03ef4e008a87909f836f
全道 影山 <mskage2022@outlook.jp>
2025-02-26 08:39:56 +0900

    list.jsの入力フィールドのセレクタを改善し、テキスト、セレクト、テキストエリアを対象にしました

--- list.js
+++ list.js
@@ -64,7 +64,9 @@ window.addEventListener('load', function () {
                 // 復号したデータをパースして各入力フィールドに反映
                 const paramsObject = Object.fromEntries(new URLSearchParams(decryptedData));
                 for (const [key, value] of Object.entries(paramsObject)) {
-                    var inputField = document.querySelector(`.kb-field[field-id="${key}"]`);// input[data-type="text"]`);
+                    // var inputField = document.querySelector(`.kb-field[field-id="${key}"] input[data-type="text"]`);
+                    // var inputField = document.querySelector(`.kb-field[field-id="${key}"]`);// input[data-type="text"]`);
+                    var inputField = document.querySelector(`.kb-field[field-id="${key}"] input, .kb-field[field-id="${key}"] select, .kb-field[field-id="${key}"] textarea`);
                     if (inputField) {
                         inputField.value = value;
                     }
