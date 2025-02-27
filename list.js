11a66b91fbb5bc067e89d81194c86a06bb46e69f
全道 影山 <mskage2022@outlook.jp>
2025-02-28 08:52:27 +0900

    子ノードの変更検出条件を修正し、テキストノードを対象に追加処理を実行する機能を実装

--- list.js
+++ list.js
@@ -212,7 +212,8 @@ window.addEventListener('load', function () {
                 console.log('子ノードの変更を検出しました');
                 // 追加された要素が指定したクラスを持つかどうかを確認
                 mutation.addedNodes.forEach(node => {
-                    if (node.nodeType === 1 && node.classList.contains(targetClass)) {
+                    if (node.nodeType === 3)  {
+                        console.log(node);
                         console.log('指定したクラスのフォームが構築されました');
                         runAdditionalProcess();
                         observer.disconnect(); // 監視を停止
