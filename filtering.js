41cca15029bd4872602f6c49e7b3957cfdc48ca1
全道 影山 <mskage2022@outlook.jp>
2025-05-16 14:18:57 +0900

    sizeTableから特定の行を取得する際のインデックスをshowidxに修正

--- filtering.js
+++ filtering.js
@@ -158,7 +158,7 @@ window.addEventListener('load', function () {
                 selectedType = "帽子";
             }
             console.log(sizeTable);
-            const rows = sizeTable[rowIdx + 1].querySelectorAll('tr'); // NodeListから特定の要素を選択
+            const rows = sizeTable[showidx + 1].querySelectorAll('tr'); // NodeListから特定の要素を選択
             rows.forEach(row => {
                 const spanElement = row.querySelector('td > div > div > span');
                 const spanText = spanElement ? spanElement.textContent : '';
