993cf54bd278f0b246b05e9e2d3ad1bf73fed674
kage2011 <kage2011_notice@yahoo.co.jp>
2025-05-16 22:59:42 +0900

    修正: addevent関数内でchangeイベントの発火位置を修正し、コードの可読性を向上

--- filtering.js
+++ filtering.js
@@ -16,11 +16,11 @@ window.addEventListener('load', function () {
             var targetElement = node.querySelector('[field-id="サイズ"] > div > input');
             targetElement.value = selectedValue;
 
+            // changeイベントを作成して発火
+            var changeEvent = new Event('click');
+            var searchbutton = node.querySelector('.kb-icon.kb-icon-lookup.kb-search')
+            searchbutton.dispatchEvent(changeEvent);
         });
-        // changeイベントを作成して発火
-        var changeEvent = new Event('click');
-        var searchbutton = node.querySelector('.kb-icon.kb-icon-lookup.kb-search')
-        searchbutton.dispatchEvent(changeEvent);
     }
 
     // オブザーバーインスタンスを生成
