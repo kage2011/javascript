e4e4e1e66f4dda0c0fb11567c402f70610646602
kage2011 <kage2011_notice@yahoo.co.jp>
2025-06-30 15:32:45 +0900

    修正: レコード作成APIのエンドポイントを更新

--- mail_assist_mobile.js
+++ mail_assist_mobile.js
@@ -264,7 +264,7 @@
                 }
             };
             
-            const response = await kintone.api(kintone.api.url('/k/m/v1/record.json', true), 'POST', body);
+            const response = await kintone.api(kintone.api.url('/k/v1/record.json', true), 'POST', body);
             
             // レコード作成画面を開く
             const recordID = parseInt(response.id);
