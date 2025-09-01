9603b1f80081ea740fbcc02facef68abfe89b964
全道 影山 <mskage2022@outlook.jp>
2025-09-01 11:43:55 +0900

    testmodeを実装

--- acceptrootautoinput.js
+++ acceptrootautoinput.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-
+  const TEST_MODE = true;
   const EMPLOYEE_APP_ID = 221;
   const FIELD_CODES = {
     MANAGER: 'manager_user', // 課長用ユーザー選択フィールド
@@ -65,57 +65,23 @@
         kaihatsuLeader.push({'code':record['社員番号'].value,'name':record['氏名'].value});
       }
     })
-    record['工機課課長'].value = kokiManager;
-    record['工機課係長'].value = kokiChief;
-    record['改善推進課課長'].value = kaizenManager;
-    record['改善推進課係長'].value = kaizenChief;
-    record['開発課課長'].value = kaihatsuManager;
-    record['開発課係長'].value = kaihatsuChief;
-    record['提出部署課長承認'].value = sectionManager;
-  //   const filtered = selfResp.records.filter(record => {
-  //     const orgName = record['組織選択']?.value?.[0]?.name || '';
-  //     const title = record['役職']?.value || '';
-
-  //     return (
-  //       (orgName.includes('工機') && (title === '課長' || title === '係長')) ||
-  //       (orgName.includes('改善推進') && (title === '課長' || title === '係長')) ||
-  //       (orgName.includes('開発') && (title === '課長' || title === '係長'))
-  //     );
-  //   });
-
-  // // 抽出した社員番号一覧を表示
-  // const employeeCodes = filtered.map(record => record['社員番号']?.value);
-  // console.log(employeeCodes);
-
-  //   const department = selfResp.records[0].department.value;
-  //   const title = selfResp.records[0].title.value;
-
-  //   // 工機課長、係長取得
-  //   // 改善推進課長、係長取得
-  //   // 開発課長、係長取得
-
-
-  //   // 3. 同じ部署で肩書が「課長」「部長」のレコードを検索
-  //   const queryLeaders = `department = "${department}" and (title = "課長" or title = "部長")`;
-  //   const leadersResp = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', {
-  //     app: EMPLOYEE_APP_ID,
-  //     query: queryLeaders
-  //   });
-
-  //   // 4. 該当するユーザーのcodeを抽出してフィールドに設定
-  //   leadersResp.records.forEach(rec => {
-  //     const leaderCode = rec.user_code.value;
-  //     const leaderName = rec.user_name.value;
-  //     const title = rec.title.value;
-
-  //     const userObj = { code: leaderCode, name: leaderName };
-
-  //     if (title === '課長') {
-  //       record[FIELD_CODES.MANAGER].value = [userObj];
-  //     } else if (title === '部長') {
-  //       record[FIELD_CODES.DIRECTOR].value = [userObj];
-  //     }
-  //   });
+    if (TEST_MODE){
+      record['工機課課長'].value = loginUser;
+      record['工機課係長'].value = loginUser;
+      record['改善推進課課長'].value = loginUser;
+      record['改善推進課係長'].value = loginUser;
+      record['開発課課長'].value = loginUser;
+      record['開発課係長'].value = loginUser;
+      record['提出部署課長承認'].value = loginUser;
+    }else{
+      record['工機課課長'].value = kokiManager;
+      record['工機課係長'].value = kokiChief;
+      record['改善推進課課長'].value = kaizenManager;
+      record['改善推進課係長'].value = kaizenChief;
+      record['開発課課長'].value = kaihatsuManager;
+      record['開発課係長'].value = kaihatsuChief;
+      record['提出部署課長承認'].value = sectionManager;
+    }
 
     return event;
   });
